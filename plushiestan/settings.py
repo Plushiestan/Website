391498f709719af27751aec29c9fd965e200b4b1
Plushiestan <fluffys.fun.adventures@gmail.com>
2020-03-26 20:43:52 +0100

    changed base_path for secret key

--- plushiestan/settings.py
+++ plushiestan/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 #securitykeyfile = os.path.join(BASE_DIR, '..', 'secret_key.txt')
-securitykeyfile = os.path.join(BASE_DIR, 'secret_key.txt')
+securitykeyfile = os.path.join(BASE_DIR, '..', 'secret_key.txt')
 with open(securitykeyfile) as f:
 	SECRET_KEY = f.read().strip()
 
